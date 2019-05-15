fd845c8ca09ac610ed1769005105661204a6dbd8
isaiahwhitfield <isaiah.whitfield@gmail.com>
2019-05-15 05:25:56 -0400

    Added delete function to the todolist and cleaned up the code

--- TodoList.cs
+++ TodoList.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-/*TodoList allows the user to view, add to and delete their todo list.
+/*
+  TodoList allows the user to view, add to and delete their todo list.
   Maybe add a difference btw logging out and quitting.
 
   add a login class(method?) that will keep a record of all of the 
@@ -16,70 +17,71 @@ public class TodoList
         bool isLoggedIn = true;
         string cmnd;
         
-        /*The while loop allows the user to make more than one change
+        /*
+          The while loop allows the user to make more than one change
           without getting logged out.
           
           Eventually this will first prompt the user to login to their profile
           or exit the program entirely
         */
-        while (isLoggedIn)
-        {
-            //Welcomes the user and introduces the function of the program.
+        while (isLoggedIn){
+            
+            /* 
+              Welcomes the user and introduces the function of the program.
+            */  
             Console.WriteLine("Add to TodoList or type delete or logout to stop");
             cmnd = Console.ReadLine();
             
-            //Maybe change this whole messy if then thing to a case switch with a String variable
+            /*Maybe change this whole messy if then thing to a case switch with a String variable
             
-            /*Right now changin the variable cmnd to logout ends the loop
+              Right now changin the variable cmnd to logout ends the loop
               and closes the program.
             
               Eventually, when I link a text file and add a user data base
               and saved data, it will just return to the welcome screen.
             */
-            if (cmnd.ToLower() == "logout")
-            {
+            if (cmnd.ToLower() == "logout"){
                 Console.WriteLine("You are logged out");
                 isLoggedIn = false;
                 break;
             }
-            /*The program prints a numbered todo list and prompts the user to enter
+            
+            /*
+              The program prints a numbered todo list and prompts the user to enter
               the number to delete an entry.
             */
             
-            else if (cmnd.ToLower() == "delete")
-            {
+            else if (cmnd.ToLower() == "delete"){
                 Console.WriteLine("Which entry? (use a number)");
                 int count = 0;
-                foreach(string listIndex in fullList)
-                {
+                foreach(string listIndex in fullList){
                     count++;
                     Console.WriteLine($"{count}.......{listIndex}");
                 }
                 cmnd = Console.ReadLine();    
                 bool isNum = Int32.TryParse(cmnd, out int delete);
-                if (isNum)
-                {
+                if (isNum){
                     fullList.RemoveAt(delete + 1);
                     fullList.ForEach(Console.WriteLine);
                     
                 } 
-                /* If the user types a string or a number that is too large they 
-                are returned to the welcome screen
+                /* 
+                  If the user types a string or a number that is too large they 
+                  are returned to the welcome screen
                 */
                 
-                else
-                {
+                else{
                     Console.WriteLine("Try Again");
                 }
             }
-            /* This adds to the todo list.
+            /* 
+              This adds to the todo list.
             
-            I do think this looks too messy and confusing with all the ifs.
-            case switch is in progress.
+              I do think this looks too messy and confusing with all the ifs.
+              case switch is in progress.
             */
             
-            else
-            {
+            else{
                 fullList.Add(cmnd);
                 fullList.ForEach(Console.WriteLine);             
             }
